1f1a02930201bcef09384b760a42ffced5277a2f
DimSkakovskyi <dimonskak@ukr.net>
2025-02-26 19:57:55 +0200

    Simple enemy AI. Going from pointA to pointB using game objects with the same names added to enemy object. No damage or player tracking AI made yet

--- Assets/Scripts/SimpleEnemyPatrol.cs
+++ Assets/Scripts/SimpleEnemyPatrol.cs
@@ -8,16 +8,52 @@ public class SimpleEnemyPatrol : MonoBehaviour
     public GameObject PointB;
     private Rigidbody2D rb;
     private Transform CurrentPoint;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        CurrentPoint = PointB.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 point = CurrentPoint.position - transform.position;
+        if (CurrentPoint == PointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2 (-speed, 0);
+        }
+
+        if(Vector2.Distance(transform.position, CurrentPoint.position) < 0.5f && CurrentPoint == PointB.transform)
+        {
+            Flip();
+            CurrentPoint = PointA.transform;
+        }
+
+        if (Vector2.Distance(transform.position, CurrentPoint.position) < 0.5f && CurrentPoint == PointA.transform)
+        {
+            Flip();
+            CurrentPoint = PointB.transform;
+        }
+    }
+
+    private void Flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(PointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(PointB.transform.position, 0.5f);
+        Gizmos.DrawLine(PointA.transform.position, PointB.transform.position);
     }
 }
